d1022f5db20fa696bcefa828f465cbc780eaf908
sujeet kumar <syadav447@rku.ac.in>
2025-03-31 18:29:42 +0530

    update code in recuriter login and navbar

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -21,25 +21,38 @@ const RecruiterLogin = () => {
         <h1 className='text-center text-2xl text-neutral-700 font-medium'>Recruiter {state}</h1>
         <p className='text-sm'>Welcome back! Please sign in to continue</p>
         <>
-            <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
+
+            {state !== 'Login' && (
+              <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
               <img src={assets.person_icon} alt="" />
-              <input onChange={e => setName(e.target.value)} value={name} type="text" placeholder='company name' required />
+              <input className='outline-none text-sm' onChange={e => setName(e.target.value)} value={name} type="text" placeholder='company name' required />
             </div>
 
+            )}
+            
             <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
               <img src={assets.email_icon} alt="" />
-              <input onChange={e => setEmail(e.target.value)} value={email} type="email" placeholder='Email Id' required />
+              <input className='outline-none text-sm' onChange={e => setEmail(e.target.value)} value={email} type="email" placeholder='Email Id' required />
             </div>
 
             <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
               <img src={assets.lock_icon} alt="" />
-              <input onChange={e => setPassword(e.target.value)} value={password} type="password" placeholder='Password' required />
+              <input className='outline-none text-sm' onChange={e => setPassword(e.target.value)} value={password} type="password" placeholder='Password' required />
             </div>
+
+            <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot password</p>
         </>
 
-        <button>
+        <button className='bg-blue-600 w-full text-white py-2 rounded-full'>
           {state === 'Login' ? 'Login' : 'create account'}
         </button>
+            {
+              state === 'Login'
+              ? <p className='mt-5 text-center'>Don't have a account? <span className='text-blue-600 cursor-pointer' onClick={()=> setState("Sign Up")}>Sign Up</span></p>
+              : <p className='mt-5 text-center'>Already have an aaccount? <span className='text-blue-600 cursor-pointer'  onClick={()=> setState("Login")}>Login</span></p>
+
+            }
+        
       </form>
     </div>
   )
