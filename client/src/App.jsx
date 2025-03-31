0ffd97ecd2eb88b14b65ca3daa75222e8a4c0956
sujeet kumar <syadav447@rku.ac.in>
2025-03-31 16:55:16 +0530

    new repo

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'; // Import useContext
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import ApplyJob from './pages/ApplyJob';
+import Applications from './pages/Applications';
+import RecruiterLogin from './components/RecruiterLogin';
+import { AppContext } from './context/AppContext';
+
+const App = () => {
+  const { showRecruiterLogin } = useContext(AppContext);
+
+  return (
+    <div>
+      {showRecruiterLogin && <RecruiterLogin />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apply-job/:id" element={<ApplyJob />} />
+        <Route path="/applications" element={<Applications />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/RecruiterLogin.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+const RecruiterLogin = () => {
+
+  const [state, setState] = useState('Login')
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [image, setImage] = useState(false)
+
+  const [isTextDataSubmited, setIsTextDataSubmited] = useState(false)
+
+    
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RecruiterLogin
+
+
+// 3:19
