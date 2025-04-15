0c1910c91e4a6373f75d24babe0abe3c0a2daa0f
sujeet kumar <syadav447@rku.ac.in>
2025-04-16 02:39:14 +0530

    create backend

---
+++ server/config/instrument.js
@@ -0,0 +1,25 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+import * as Sentry from "@sentry/node"
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: "https://d709883352ddffe1c998786c346c0a77@o4509156415700992.ingest.us.sentry.io/4509156428611584",
+  integrations: [
+    nodeProfilingIntegration(),
+    Sentry.mongooseIntegration()
+  ],
+  // Tracing
+//   tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is evaluated only once per SDK.init call
+  profileSessionSampleRate: 1.0,
+  // Trace lifecycle automatically enables profiling during active traces
+  profileLifecycle: 'trace',
+});
+
+// Profiling happens automatically after setting it up with `Sentry.init()`.
+// All spans (unless those discarded by sampling) will have profiling data attached to them.
+Sentry.startSpan({
+  name: "My Span",
+}, () => {
+  // The code executed here will be profiled
+});
