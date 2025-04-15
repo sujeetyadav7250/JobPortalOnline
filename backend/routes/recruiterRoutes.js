ea0fbc0276cd166f04f1dc357e9bdf8db86abdd2
sujeetyadav7250 <sy340190@gmail.com>
2025-04-16 02:47:39 +0530

    Delete backend directory

--- backend/routes/recruiterRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { registerRecruiter, loginRecruiter } = require("../controllers/recruiterController");
-
-router.post("/signup", registerRecruiter);
-router.post("/login", loginRecruiter);
-
-module.exports = router;
